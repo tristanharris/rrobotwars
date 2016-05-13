942c101a682d7a532a9f9d96067c694725fa69c4
Tristan Harris <tristan@sticktech.co.uk>
2016-05-13 12:57:35 +0100

    Turn_to command

--- react.rb
+++ react.rb
@@ -3,21 +3,45 @@ require 'rrobots'
 class React
   include Robot
 
+  attr :pending
+
+  def initialize
+    @pending = {}
+    turn_to(90, 2)
+  end
+
   def tick events
-    if time == 0
-      @run = 0
+    #turn_gun 2
+    #fire 3 unless events['robot_scanned'].empty?
+    #accelerate(-1) if velocity > 0 && @run < 0
+    #if !events['got_hit'].empty?
+    #  accelerate 8
+    #  turn 10
+    #  @run = 300
+    #end
+    run_actions
+  end
+
+  def run_actions
+    if pending[:turn]
+      angle = pending[:turn][:angle]
+      if heading == angle
+        finish :turn
+      else
+        step = pending[:turn][:step]
+        dir = ((heading - angle) % 360 - (angle - heading) % 360) <=> 0
+        step = [step, (heading - angle).abs].min
+        turn dir * step
+      end
     end
-    @run -= 1
-    turn_gun 2
-    turn 10 if @run % 50 == 0
-    say velocity
-    fire 3 unless events['robot_scanned'].empty?
-    accelerate(-1) if velocity > 0 && @run < 0
-    if !events['got_hit'].empty?
-      accelerate 8
-      turn 10
-      @run = 300
-    end
+  end
+
+  def turn_to(angle, step)
+    @pending[:turn] = {angle: angle, step: step}
+  end
+
+  def finish(action)
+    @pending[action] = nil
   end
 
 end
