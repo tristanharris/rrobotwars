f968af2af943666f85dbc7881b53a422bc6e16f1
Tristan Harris <tristan@sticktech.co.uk>
2016-05-13 13:13:26 +0100

    Accelerate_to command

--- react.rb
+++ react.rb
@@ -8,12 +8,12 @@ class React
   def initialize
     @pending = {}
     turn_to(90, 2)
+    accelerate_to 8
   end
 
   def tick events
     #turn_gun 2
     #fire 3 unless events['robot_scanned'].empty?
-    #accelerate(-1) if velocity > 0 && @run < 0
     #if !events['got_hit'].empty?
     #  accelerate 8
     #  turn 10
@@ -34,10 +34,23 @@ class React
         turn dir * step
       end
     end
+    if pending[:accelerate]
+      speed = pending[:accelerate][:speed]
+      if velocity == speed
+        finish(:accelerate)
+      else
+        dir = (speed - velocity) <=> 0
+        accelerate dir
+      end
+    end
   end
 
   def turn_to(angle, step)
     @pending[:turn] = {angle: angle, step: step}
+  end
+
+  def accelerate_to(speed)
+    @pending[:accelerate] = {speed: speed}
   end
 
   def finish(action)
